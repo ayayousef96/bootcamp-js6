c75c98528c04ad5298503f23a8867817fdce86a1
ayayousef96 <ayayou1205@gmail.com>
2022-10-11 23:45:23 +0300

    18.2

---
+++ 18.2/app.js
@@ -0,0 +1,11 @@
+const h1=document.querySelector('h1');
+h1.style.color= 'blue';
+const p =document.querySelector('p');
+p.style.fontSize= '30px';
+const d = document.getElementById("description");
+d.style.display = 'inline-block';
+const body =document.querySelector('body');
+body.style.backgroundColor ='green';
+const t=document.querySelector('title');
+t.innerText="New Title";
+
